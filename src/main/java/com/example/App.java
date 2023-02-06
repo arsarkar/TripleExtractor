98748ead97f5779c4960cf878c01bdf861dd3567
Arkopaul Sarkar <as888211@ohio.edu>
2023-02-06 22:27:24 +0100

    initial commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,44 @@
+package com.example;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Model m = ModelFactory.createDefaultModel();
+        try {
+            m.read(new FileInputStream(new File(args[0])), null, "RDF/XML");
+            // m.write(System.out, "TTL");
+            System.out.println("=========================================");
+            StmtIterator  siter = m.listStatements();
+            FileOutputStream fo = new FileOutputStream(new File(args[1]));
+            while(siter.hasNext()){
+                Statement s = siter.next();
+                // System.out.println(s.getSubject().toString() + "\t" +
+                //                                 s.getPredicate().toString() + "\t" +
+                //                                 s.getObject().toString());
+                fo.write(new String(s.getSubject().toString() + " " +
+                s.getPredicate().toString() + " " +
+                s.getObject().toString() + "\n").getBytes());
+            }
+            fo.flush();
+            fo.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
